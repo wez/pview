83ff160284a319985093f1649c3194dcbaab15a0
Wez Furlong <wez@wezfurlong.org>
2023-12-27 20:57:38 -0700

    flesh out some more details for the addon

---
+++ addon/README.md
@@ -0,0 +1,10 @@
+# PowerView to MQTT bridge for Home Assistant
+
+This addon provides a bridge between a [Hunter Douglas PowerView
+Hub](https://www.hunterdouglas.com/operating-systems/motorized/powerview) and
+Home Assistant, via the [Home Assistant MQTT
+Integration](https://www.home-assistant.io/integrations/mqtt/).
+
+Once you have configured your MQTT service credentials, this addon should
+automatically discover your PowerView Hub and import its shades and scenes
+via the MQTT integration.
--- addon/run.sh
+++ addon/run.sh
@@ -3,28 +3,36 @@
 export RUST_BACKTRACE=full
 export RUST_LOG_STYLE=always
 
+if bashio::services.available mqtt ; then
+  export PV_MQTT_HOST="$(bashio::services mqtt 'host')"
+  export PV_MQTT_PORT="$(bashio::services mqtt 'port')"
+  export PV_MQTT_USER="$(bashio::services mqtt 'username')"
+  export PV_MQTT_PASSWORD="$(bashio::services mqtt 'password')"
+fi
+
+if bashio::config.has_value hub_ip ; then
+  export PV_HUB_IP="$(bashio::config hub_ip)"
+fi
+
 if bashio::config.has_value mqtt_host ; then
   export PV_MQTT_HOST="$(bashio::config mqtt_host)"
-else
-  export PV_MQTT_HOST="$(bashio::services mqtt 'host')"
 fi
+
 
 if bashio::config.has_value mqtt_port ; then
   export PV_MQTT_PORT="$(bashio::config mqtt_port)"
-else
-  export PV_MQTT_PORT="$(bashio::services mqtt 'port')"
 fi
 
 if bashio::config.has_value mqtt_username ; then
   export PV_MQTT_USER="$(bashio::config mqtt_username)"
-else
-  export PV_MQTT_USER="$(bashio::services mqtt 'username')"
 fi
 
 if bashio::config.has_value mqtt_pass ; then
   export PV_MQTT_PASSWORD="$(bashio::config mqtt_password)"
-else
-  export PV_MQTT_PASSWORD="$(bashio::services mqtt 'password')"
+fi
+
+if bashio::config.has_value debug_level ; then
+  export RUST_LOG=pview="$(bashio::config debug_level)"
 fi
 
 env | grep PV_
