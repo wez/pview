081289a1eec3770110122eed99f6cee5eb9342aa
Wez Furlong <wez@wezfurlong.org>
2023-12-27 20:10:03 -0700

    update addon
    This seems to get things booting for me, in my ham-strung test
    setup; in a devcontainer that can't access either my mqtt broker
    or my pv hub.
    
    I can see the output from run.sh and error messages from pview
    when it fails to discover my hub.
    
    refs: https://github.com/wez/pview/issues/1

--- addon/run.sh
+++ addon/run.sh
@@ -1,3 +1,32 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Hello world!"
+export RUST_BACKTRACE=full
+export RUST_LOG_STYLE=always
+
+if bashio::config.has_value mqtt_host ; then
+  export PV_MQTT_HOST="$(bashio::config mqtt_host)"
+else
+  export PV_MQTT_HOST="$(bashio::services mqtt 'host')"
+fi
+
+if bashio::config.has_value mqtt_port ; then
+  export PV_MQTT_PORT="$(bashio::config mqtt_port)"
+else
+  export PV_MQTT_PORT="$(bashio::services mqtt 'port')"
+fi
+
+if bashio::config.has_value mqtt_username ; then
+  export PV_MQTT_USER="$(bashio::config mqtt_username)"
+else
+  export PV_MQTT_USER="$(bashio::services mqtt 'username')"
+fi
+
+if bashio::config.has_value mqtt_pass ; then
+  export PV_MQTT_PASSWORD="$(bashio::config mqtt_password)"
+else
+  export PV_MQTT_PASSWORD="$(bashio::services mqtt 'password')"
+fi
+
+env | grep PV_
+
+exec /pview serve-mqtt
